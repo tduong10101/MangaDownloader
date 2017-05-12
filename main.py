b027a2d436b109101a9094dbc5fd8276565f448e
Terry Duong <tduong10101@gmail.com>
2017-05-12 23:17:38 +1000

    Divided main to functions
    Create get_chapters, get_pages, make_dir and down_img
    
    Signed-off-by: Terry Duong <tduong10101@gmail.com>

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-import BeautifulSoup as bs
-import urllib
-import requests
-import time
-import os
-
-address = "http://mangafox.me/manga/hana_haru/c001/1.html"
-url = requests.get(address)
-soup = bs.BeautifulSoup(url.content)
-list = []
-for o in soup.find("select",attrs={"class":"m"}):
-    list.append(o)
-list = [x for x in list if x != "\n" and x !=" " and "Comments" not in x]
 # TODO tidy up the code
 # TODO create UI
 # TODO read main manga page and pull out how many chapter available
 # TODO function to choose download multiple chapters - number of chapter / all
-print len(list)
-print list
-for i in range(1,len(list)+1):
-    link = "{}{}.html".format(address[:-6],i)
-    split = (address.split("/"))
+import os
+import urllib
+
+import BeautifulSoup as bs
+import requests
+
+address = "http://mangafox.me/manga/hetalia_world_stars/"
+
+
+def get_chapters(addr):
+    url = requests.get(addr)
+    soup = bs.BeautifulSoup(url.content)
+    list = []
+    ul = soup.find("ul", attrs={"class": "chlist"})
+    for a in ul.findAll("a", attrs={"class": "tips"}):
+        # clink = c.find("a",attrs={"class":"tips"})
+        # print clc[]ink
+        list.append(a['href'])
+    return list
+
+
+def get_pages(addr):
+    url = requests.get(addr)
+    soup = bs.BeautifulSoup(url.content)
+    list = []
+
+    select = soup.find("select", attrs={"class": "m"})
+    length = len(select.findAll("option"))
+    split = addr.split("/")
+    lastSl = split[-1]
+    print lastSl
+    print length
+    for i in range(1, length):
+        pageLink = addr.replace(lastSl, "{}.html".format(i))
+        list.append(pageLink)
+    return list
+
+
+def make_dir(addr):
+    split = addr.split("/")
     chap = split[-2]
     name = split[-3]
-    page = i
-
-    filename = "{} {} {}".format(name,chap,page)
-    #urllib.urlretrieve(link, "test.jpg")
-    print link
-    print filename
-
     dir = os.path.dirname(__file__)
-
-    dir =  "{}/{}/{}".format(dir, name, chap)
-
+    dir = "{}/{}/{}".format(dir, name, chap)
     if not os.path.exists(dir):
         os.makedirs(dir)
-    dir = "{}/{}.jpg".format(dir,page)
-    print dir
-    url = requests.get(link)
-    soup = bs.BeautifulSoup(url.content)
-    img = soup.find("img",attrs={"id":"image"})
+        return dir
+    else:
+        return False
 
-    urllib.urlretrieve(img['src'], dir)
+
+def down_img(addr, filepath):
+    url = requests.get(addr)
+    soup = bs.BeautifulSoup(url.content)
+    img = soup.find("img", attrs={"id": "image"})
+    return urllib.urlretrieve(img['src'], filepath)
 
 #link = "http://h.mfcdn.net/store/manga/25133/001.0/compressed/d000.jpg?token=bf1ccec29786275fb5e6664a766e38db&ttl=1494482400"
 #urllib.urlretrieve(link,"test.jpg")
